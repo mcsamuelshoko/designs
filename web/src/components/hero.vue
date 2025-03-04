09e3eae2d27c13c786142fdc65fa71811de6ea4a
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2025-03-04 14:37:02 +0200

    feat: create hero.vue

---
+++ web/src/components/hero.vue
@@ -0,0 +1,30 @@
+<style lang="ts" setup>
+</style>
+
+<template>
+    <section id="heroLg" class="hidden md:block">
+        <div class="h-[25vh] border-b-gray-50 flex justify-between">
+            <p class="capitalize text-8xl">Mc Samuel</p>
+            <p class="capitalize">Brand Design</p>
+        </div>
+        <div class="h-[25vh] border-b-gray-50 flex justify-between">
+            <p class="">UI/UX Design</p>
+            <p class="capitalize">Eeb Design</p>
+            <p class="capitalize text-8xl">Shoko</p>
+        </div>
+        <div class="h-[25vh] border-b-gray-50 flex justify-between">
+            <p class="capitalize text-8xl">Digital</p>
+            <p class="capitalize">Logo Identity</p>
+        </div>
+        <div class="h-[25vh] border-b-gray-50 flex justify-between">
+            <div>
+                img:s*3
+            </div>
+            <p class="capitalize text-8xl">Designer</p>
+        </div>
+        <div class="h-[25vh] border-b-gray-50 flex justify-between"></div>
+    </section>
+    <section id="heroSm" class="md:hidden">
+            for small screens
+    </section>
+</template>
