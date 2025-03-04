6f5dd712919c09c131e06f85b75bde661e808c79
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2025-03-04 14:18:34 +0200

    feat: create header.vue

---
+++ web/src/components/header.vue
@@ -0,0 +1,14 @@
+<template>
+    <header>
+            <ul class="">
+                <li class="capitalize"><a href="/">Mc Samuel Shoko</a></li>
+                <li><a href="/work">work</a></li>
+                <li><a href="/contact">Contact</a></li>
+                <li><a href="/about">About</a></li>
+            </ul>
+    </header>
+</template>
+
+<script>
+
+</script>
