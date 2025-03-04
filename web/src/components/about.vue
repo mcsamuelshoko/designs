cecd3a656f67ae3fadb8522a44782243f7e234ca
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2025-03-04 14:56:53 +0200

    feat: create about.vue

---
+++ web/src/components/about.vue
@@ -0,0 +1,30 @@
+<script lang="en" setup>
+</script>
+
+<template>
+    <section id="bio" class="border-x">
+        <div class="border-x border-dashed mx-40 px-8 flex align-center">
+            <p>
+                I take your <span>ideas</span> and draw them with clear shapes and smart designs. <span><img src="star svg" alt="star shape"></span>
+            </p>
+        </div>
+    </section>
+    <section id="skills">
+        <div class="border-b">
+            I make <span>websites,</span> apps, branding, logos, ads
+        </div>
+        <div class="border-b">
+            <span class="text-black/0">I make websites,</span> that look good and work great.
+        </div>
+        <div class="border-b grid grid-cols-2 md:grid-cols-4">
+            <div class="flex justify-center items-center border-r">I</div>
+            <div class="flex justify-center items-center md:border-r">II</div>
+            <div class="flex justify-center items-center border-r">III</div>
+            <div class="flex justify-center items-center">IV</div>
+        </div>
+
+    </section>
+    <section id="likes">
+
+    </section>
+</template>
