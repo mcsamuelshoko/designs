873aa7a9d2e58c388b6fb81667b7bd64ca6afecd
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2025-03-04 15:33:06 +0200

    feat: create contact.vue

---
+++ web/src/components/contact.vue
@@ -0,0 +1,36 @@
+<style lang="ts" setup>
+
+</style>
+
+<template>
+<section id="contact">
+    <div class="border-b grid md:grid-cols-2">
+        <p class="md:border-r border-b md:border-b-0 p-8">Every pixel has a purpose in my work.
+I focus on clean designs that help
+your business grow and make
+customers happy.</p>
+        <p class="p-8">I turn messy thoughts into sharp
+designs. Ideas get a clean look that
+grabs attention and works. My designs
+don't only look good—they solve
+problems and help you reach your
+goals.</p>
+    </div>
+    <div class="border-b grid grid-cols-2 p-8">
+        <div>
+            <p>
+                Let&apos;s turn your big <b>ideas</b> into <br>
+                something real. <span>&xrarr;</span>
+            </p>
+        </div>
+        <ul class="flex space-x-2 px-8">
+            <li><a href="https://www.linkedin.com/in/mc-samuel-shoko-2483731b5" title="visit LinkedIn">Linkedin</a></li>
+            <li><a href="https://twitter.com/mcsamuelshoko" title="Visit X(Twitter)">X (Twitter)</a></li>
+            <li><a href="https://discord.com/users/generalseven" title="Visit Discord">Discord</a></li>
+            <li><a href="https://github.com/mcsamuelshoko" title="Visit Github">Github</a></li>
+            <li><a href="https://instagram.com/mcsamuelshoko" title="Visit Instagram">Instagram</a></li>
+            <li><a href="https://linktr.ee/mcsamuelshoko" title="visit Linktree profile">Linktree</a></li>
+        </ul>
+    </div>
+</section>
+</template>
