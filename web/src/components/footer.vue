d12f03baf91762f8db5fbde0b47034b1f8072df6
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2025-03-06 15:05:48 +0200

    finished footer.vue

--- web/src/components/footer.vue
+++ web/src/components/footer.vue
@@ -1,50 +1,53 @@
-<script setup lang="ts">
-    const currentYear =1 ;
+<script lang="ts" setup>
+const currentYear = new Date().getFullYear();
+import SubMark from '../assets/submark.svg'
 </script>
 
 <template>
     <footer>
-        <div class="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1">
-            <div class="flex align-center justify-center">
-                <img src="" alt="sub-mark logo">
+        <div class="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 border-b bg-accent lg:h-[40vh]">
+            <div class="flex align-center justify-center items-center md:border-r md:border-dashed border-white">
+                <img src="/favicon.svg" class="h-36" alt="sub-mark logo">
+                <!-- <SubMark class="h-6 bg-gree-500" /> -->
             </div>
             <div class="flex flex-col">
-                <p class="uppercase">Contact</p>
-                <ul>
-                    <li><a href="https://www.linkedin.com/in/mc-samuel-shoko-2483731b5" title="visit LinkedIn">Linkedin</a></li>
-                    <li><a href="https://twitter.com/mcsamuelshoko" title="Visit X(Twitter)">X (Twitter)</a></li>
-                    <li><a href="https://discord.com/users/generalseven" title="Visit Discord">Discord</a></li>
-                    <li><a href="https://github.com/mcsamuelshoko" title="Visit Github">Github</a></li>
-                    <li><a href="https://instagram.com/mcsamuelshoko" title="Visit Instagram">Instagram</a></li>
-                    <li><a href="https://linktr.ee/mcsamuelshoko" title="visit Linktree profile">Linktree</a></li>
+                <p class="uppercase bg-primary px-4 pt-8 pb-2 font-display text-2xl">Contact</p>
+                <ul class="flex-1 p-4  lg:border-r lg:border-dashed border-white  space-y-1">
+                    <li><a target="_blank" class="hover:bg-black transition-all underline hover:no-underline p-1 hover:px-4 hover:rounded-4xl" href="https://www.linkedin.com/in/mc-samuel-shoko-2483731b5"
+                            title="visit LinkedIn">Linkedin&nbsp;&nearrow;</a></li>
+                    <li><a target="_blank" class="hover:bg-black transition-all underline hover:no-underline p-1 hover:px-4 hover:rounded-4xl" href="https://twitter.com/mcsamuelshoko" title="Visit X(Twitter)">Twitter&nbsp;&nearrow;</a></li>
+                    <li><a target="_blank" class="hover:bg-black transition-all underline hover:no-underline p-1 hover:px-4 hover:rounded-4xl" href="https://discord.com/users/generalseven" title="Visit Discord">Discord&nbsp;&nearrow;</a></li>
+                    <li><a target="_blank" class="hover:bg-black transition-all underline hover:no-underline p-1 hover:px-4 hover:rounded-4xl" href="https://github.com/mcsamuelshoko" title="Visit Github">Github&nbsp;&nearrow;</a></li>
+                    <li><a target="_blank" class="hover:bg-black transition-all underline hover:no-underline p-1 hover:px-4 hover:rounded-4xl" href="https://instagram.com/mcsamuelshoko" title="Visit Instagram">Instagram&nbsp;&nearrow;</a></li>
+                    <li><a target="_blank" class="hover:bg-black transition-all underline hover:no-underline p-1 hover:px-4 hover:rounded-4xl" href="https://linktr.ee/mcsamuelshoko" title="visit Linktree profile">Linktree&nbsp;&nearrow;</a></li>
                 </ul>
 
             </div>
             <div class="flex flex-col">
-                <p class="uppercase">Credits</p>
-                <ul>
-                    <li><a href="https://www.linkedin.com/in/mc-samuel-shoko-2483731b5" title="visit LinkedIn">Linkedin</a></li>
-                    <li><a href="https://twitter.com/mcsamuelshoko" title="Visit X(Twitter)">X (Twitter)</a></li>
-                    <li><a href="https://discord.com/users/generalseven" title="Visit Discord">Discord</a></li>
-                    <li><a href="https://github.com/mcsamuelshoko" title="Visit Github">Github</a></li>
-                    <li><a href="https://instagram.com/mcsamuelshoko" title="Visit Instagram">Instagram</a></li>
-                    <li><a href="https://linktr.ee/mcsamuelshoko" title="visit Linktree profile">Linktree</a></li>
+                <p class="uppercase bg-primary px-4 pt-8 pb-2 font-display text-2xl">Credits</p>
+                <ul class="flex-1 p-4 md:border-r md:border-dashed border-white  space-y-1">
+                    <li><p>Portfolio inspiration &mdash; French designer Maelan Lemeur via LapaNinja</p></li>
+                    <li><p>Icons &mdash; Carbon Icons by IBM</p></li>
+                    <li><br></li>
+                    <li><p>Design &mdash; Mc Samuel Shoko, in Figma</p></li>
+                    <li><p>Developer &mdash; Mc Samuel Shoko, in VSCode</p></li>
                 </ul>
             </div>
             <div class="flex flex-col">
-                <p class="uppercase">Website stack</p>
-                <ul class="flex-1 flex flex-col">
-                    <li><a href="https://www.linkedin.com/in/mc-samuel-shoko-2483731b5" title="visit LinkedIn">Astro</a></li>
+                <p class="uppercase bg-primary px-4 pt-8 pb-2 font-display text-2xl">Website stack</p>
+                <ul class="flex-1 flex flex-col p-4 space-y-1">
+                    <li><a href="https://www.linkedin.com/in/mc-samuel-shoko-2483731b5" title="visit LinkedIn">Astro</a>
+                    </li>
                     <li><a href="https://twitter.com/mcsamuelshoko" title="Visit X(Twitter)">Vue.js</a></li>
                     <li><a href="https://discord.com/users/generalseven" title="Visit Discord">Tailwind Css</a></li>
-                    <li class="flex-1"></li>
+                    <li class="h-4"></li>
                     <li><a href="https://html.com/" title="Visit Html5">Html5</a></li>
                     <li><a href="https://www.typescriptlang.org/" title="Visit Typescript">Typescript</a></li>
                     <li><a href="https://www.w3.org/Style/CSS/Overview.en.html" title="visit Css">Css</a></li>
                 </ul>
             </div>
         </div>
-        <div class="">
+        <div class="flex justify-center items-center h-12 font-display text-2xl">
             <p class="uppercase">&copy; All rights reserved &mdash; {{ currentYear }}</p>
         </div>
     </footer>
