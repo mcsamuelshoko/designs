0f40184a5354d76981b0c68dbc1bd8fcaa19c5d3
Mc Samuel Shoko <66551316+Stroustrups-Sentinel@users.noreply.github.com>
2025-03-07 14:09:45 +0200

    update on button.vue, refined & finished the component

--- web/src/components/button.vue
+++ web/src/components/button.vue
@@ -1,8 +1,20 @@
 <script lang="ts" setup>
 
+const { title, url, target } = defineProps<{
+    title: string,
+    url: string,
+    target?: "blank" | "self",
+}>()
+
+const openTarget = (s: "blank" | "self" | undefined) => (s == "blank" ? "_blank" : "_self");
+
+
+
+
 </script>
 <template>
-    <a href="">
-        <button class="rounded-lg uppercase bg-blue-700" type="button">More &ast;</button>
+    <a :href="url" :target="openTarget(target)">
+        <button class="rounded-full uppercase bg-blue-700 p-1 px-8 text-xl  hover:opacity-80 hover:cursor-pointer"
+            type="button">{{ title }}</button>
     </a>
 </template>
