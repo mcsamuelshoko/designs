15a73517f8a6ba4be1227e8a1610e69b4607b498
Mc Samuel Shoko <66551316+mcsamuelshoko@users.noreply.github.com>
2025-07-10 14:30:00 +0200

    update footer.vue,

--- web/src/components/footer.vue
+++ web/src/components/footer.vue
@@ -9,19 +9,20 @@ const currentYear = new Date().getFullYear();
 <template>
     <footer class="border-t">
         <div class="grid lg:grid-cols-4 md:grid-cols-2 grid-cols-1 border-b bg-accent lg:min-h-[40vh]">
-            <div class="bg-primary md:bg-transparent flex align-center justify-center items-center md:border-r md:border-dashed border-white">
+            <div class="bg-primary md:bg-transparent flex align-center justify-center items-center md:border-r md:border-solid border-white">
                 <img :src="`${baseUrl}/favicon.svg`" class="h-36" alt="sub-mark logo">
                 <!-- <SubMark class="h-6 bg-gree-500" /> -->
             </div>
             <div class="flex flex-col">
                 <p class="uppercase bg-primary px-4 pt-8 pb-2 font-display text-2xl">Contact</p>
-                <ul class="flex-1 p-4  lg:border-r lg:border-dashed border-white  space-y-1 uppercase">
+                <ul class="flex-1 p-4  lg:border-r lg:border-solid border-white  space-y-1 uppercase">
                     <li><a target="_blank" class="hover:bg-black transition-all  hover:no-underline p-1 hover:px-4 hover:rounded-4xl" href="https://www.linkedin.com/in/mc-samuel-shoko-2483731b5"
                             title="visit LinkedIn">Linkedin</a></li>
                     <li><a target="_blank" class="hover:bg-black transition-all  hover:no-underline p-1 hover:px-4 hover:rounded-4xl" href="https://behance.com/mcsamuelshoko" title="Visit Behance">Behance</a></li>
                     <li><a target="_blank" class="hover:bg-black transition-all  hover:no-underline p-1 hover:px-4 hover:rounded-4xl" href="https://dribbble.com/mcsamuelshoko" title="Visit Github">Dribble</a></li>
                     <li><a target="_blank" class="hover:bg-black transition-all  hover:no-underline p-1 hover:px-4 hover:rounded-4xl" href="https://twitter.com/mcsamuelshoko" title="Visit X(Twitter)">Twitter</a></li>
                     <li><a target="_blank" class="hover:bg-black transition-all  hover:no-underline p-1 hover:px-4 hover:rounded-4xl" href="https://discord.com/users/generalseven" title="Visit Discord">Discord</a></li>
+                    <li><a target="_blank" class="hover:bg-black transition-all  hover:no-underline p-1 hover:px-4 hover:rounded-4xl" href="https://gitlab.com/mcsamuelshoko" title="Visit Gitlab">Gitlab</a></li>
                     <li><a target="_blank" class="hover:bg-black transition-all  hover:no-underline p-1 hover:px-4 hover:rounded-4xl" href="https://github.com/mcsamuelshoko" title="Visit Github">Github</a></li>
                     <li><a target="_blank" class="hover:bg-black transition-all  hover:no-underline p-1 hover:px-4 hover:rounded-4xl" href="https://instagram.com/mcsamuelshoko" title="Visit Instagram">Instagram</a></li>
                     <li><a target="_blank" class="hover:bg-black transition-all  hover:no-underline p-1 hover:px-4 hover:rounded-4xl" href="https://linktr.ee/mcsamuelshoko" title="visit Linktree profile">Linktree</a></li>
@@ -30,7 +31,7 @@ const currentYear = new Date().getFullYear();
             </div>
             <div class="flex flex-col">
                 <p class="uppercase bg-primary px-4 pt-8 pb-2 font-display text-2xl">Credits</p>
-                <ul class="flex-1 p-4 md:border-r md:border-dashed border-white  space-y-2">
+                <ul class="flex-1 p-4 md:border-r md:border-solid border-white  space-y-2">
                     <li><p><span class="uppercase font-bold">Portfolio inspiration</span> <br> &mdash; French designer Maelan Lemeur via LapaNinja</p></li>
                     <li><p><span class="uppercase font-bold">Icons</span> <br> &mdash; Carbon Icons by IBM</p></li>
                     <li><br></li>
